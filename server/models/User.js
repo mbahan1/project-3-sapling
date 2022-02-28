0d57b27ee6e251bc83eaf44532a7bc3a72edf8ef
helenamarkus14 <helenamarkus14@gmail.com>
2022-02-28 13:35:02 -0700

    figuring out models/schemas

---
+++ server/models/User.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+const Post = require("./Post");
+const Schema = mongoose.Schema;
+
+
+const userSchema = new Schema({
+   firstName: {
+       type: String,
+       required: true,
+       unique: true,
+   },
+   lastName: {
+       type: String,
+       required: true,
+       unique: true,
+   },
+   //email as username?
+   email: {
+       type: String,
+       required: true,
+       unique: true,
+   },
+   password: {
+       type: String, 
+       required: true,
+       //specifies default path selection behavior aka false excludes "password" from query results by default
+       select: false
+   },
+   //google oauth
+   googleId : {type: String},
+   bio: {type: String}, //required?
+   age: {type: Number},
+   pronouns: {type: String},
+   zodiacSign: {type: String, required: true},
+   horoscopeApi: {type: String}, //type depends on what API we choose
+   posts: [Post], //change to manifest?
+   comments: [Comment]
+});
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;

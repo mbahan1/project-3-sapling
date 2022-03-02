6feb57cabb9916155afc91687351cff5eeb41db5
jihyekwak <jihyekwak53@gmail.com>
2022-03-01 21:24:37 -0800

    updateComment fixed

--- server/routes/comments.js
+++ server/routes/comments.js
@@ -4,7 +4,7 @@ const { comments } = require("../controllers")
 router.get("/:id/comments", comments.index)
 // router.get("/:id", comments.show)
 router.post("/:id/comments", comments.create)
-router.put("/:id/comments/:id", comments.update)
+router.put("/:id/comments/:commentid", comments.update)
 router.delete("/:id/comments/:id", comments.destroy)
 
 module.exports = router;

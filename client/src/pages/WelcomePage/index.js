023e5a65acb748105d1c49d2a7b7a74926ab3f60
jihyekwak <jihyekwak53@gmail.com>
2022-03-02 13:27:45 -0800

    welcomePage,  LoginComponent, BrowserRouter

--- client/src/pages/WelcomePage/index.js
+++ client/src/pages/WelcomePage/index.js
@@ -3,7 +3,6 @@
 // Manifest app description Component
 // Link to RegisterPage
 
-import {Link} from "react-router-dom"
 import LoginForm from "../../components/LoginForm"
 
 export default function WelcomePage() {
@@ -11,7 +10,6 @@ export default function WelcomePage() {
         <div>
             <h3>[WELCOME PAGE]</h3>
             <LoginForm />
-            <Link to="/signUp">Sign up</Link>
         </div>
     )
 }

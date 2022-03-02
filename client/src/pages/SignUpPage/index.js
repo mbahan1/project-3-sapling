787814fda22d16e29e6c4da889457123ddd97bb3
jihyekwak <jihyekwak53@gmail.com>
2022-03-02 14:26:38 -0800

    created signUpComponent, signUpPage

---
+++ client/src/pages/SignUpPage/index.js
@@ -0,0 +1,13 @@
+// Signup Page contains
+// SignUpForm Component
+
+import SignUpForm from "../../components/SignUpForm";
+
+export default function SignUpPage() {
+    return (
+        <div>
+            <h3>[SIGNUP PAGE]</h3>
+            <SignUpForm />
+        </div>
+    )
+}

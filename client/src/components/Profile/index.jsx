e0f49c8a838f037ad5e767872da8cdb11a4c7f36
neshmasood <neshtam@gmail.com>
2022-03-04 17:46:40 -0600

    minor change in the userEvent import

--- client/src/components/Profile/index.jsx
+++ client/src/components/Profile/index.jsx
@@ -1,4 +1,4 @@
-import userEvent from "@testing-library/user-event";
+// import userEvent from "@testing-library/user-event";
 import { string } from "prop-types";
 import { array } from "prop-types";
 import React from "react";

1b29340fbac8a254c1857c07de1a79f1f2562781
jihyekwak <jihyekwak53@gmail.com>
2022-03-04 18:14:41 -0800

    created profileEditPage, update user model, update user.service.js

---
+++ client/src/components/ProfileEditForm/index.jsx
@@ -0,0 +1,103 @@
+import {useState} from "react";
+import * as userService from "../../api/user.service";
+
+
+export default function ProfileEditPage() {
+
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [pronouns, SetPronouns] = useState("");
+    const [age, setAge] = useState("");
+    const [bio, setBio] = useState("");
+    const [hobbies, setHobbies] = useState("");
+    const [zodiacSign, setZodiacSign] = useState("")
+
+    const handleSubmit = async () => {
+        let updatedUser = {firstName, lastName, pronouns, hobbies, zodiacSign};
+        let res = await userService.update(updatedUser).then(() => {
+            console.log(updatedUser);
+        })
+
+        console.log(res);
+        if (!res === 201) {
+            alert(`ERROR code: ${res.status}`)
+        }
+    }
+    return(
+        <div className="profileEditFormComponent component">
+            <form>
+                <label htmlFor="firtName">First Name</label>
+                <input 
+                    onChange={(e)=> setFirstName(e.target.value)}
+                    value={firstName}
+                    type="text"
+                    name="firstName"
+                    placeholder="First Name"  
+                />
+
+                <label htmlFor="lastName">Last Name</label>
+                <input 
+                    onChange={(e)=> setLastName(e.target.value)}
+                    value={lastName}
+                    type="text"
+                    name="lastName"
+                    placeholder="Last Name"
+                />
+
+                <label htmlFor="email">Pronouns</label>
+                <input
+                    onChange={(e)=> SetPronouns(e.target.value)}
+                    value={pronouns}
+                    type="text"
+                    name="email"
+                    placeholder="email"
+                />
+
+                <label htmlFor="age">Age</label>
+                <input
+                    onChange={(e)=> setAge(e.target.value)}
+                    value={age}
+                    type="text"
+                    name="email"
+                    placeholder="email"
+                />
+
+                <label htmlFor="bio">Bio</label>
+                <input
+                    onChange={(e)=> setBio(e.target.value)}
+                    value={bio}
+                    type="text"
+                    name="email"
+                    placeholder="email"
+                />
+
+                <label htmlFor="hobbies">Hobbies</label>
+                <input
+                    onChange={(e)=> setHobbies(e.target.value)}
+                    value={hobbies}
+                    type="text"
+                    name="password"
+                    placeholder="Password"
+                />
+
+                <label htmlFor="zodiacSign">Zodiac Sign</label>
+                <input 
+                    onChange={(e)=> setZodiacSign(e.target.value)}
+                    value={zodiacSign}
+                    type="text"
+                    name="zodiacSign"
+                    placeholder="Zodiac Sign"
+                />
+
+                <select>
+                    <option>Select Zodiac Sign</option>
+                </select>
+                <button 
+                    onClick={handleSubmit}
+                > Submit
+                </button>
+            </form>
+
+        </div>
+    )
+}
---
+++ client/src/pages/ProfileEditPage/index.js
@@ -0,0 +1,10 @@
+import ProfileEditForm from "../../components/ProfileEditForm"
+
+export default function ProfileEditPage() {
+    return(
+        <>
+        <h3>Edit Profile</h3>
+        <ProfileEditForm />
+        </>
+    )
+}

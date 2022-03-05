4e1e76647d852f074f8dabe8629341a11d67f5cc
helenamarkus14 <helenamarkus14@gmail.com>
2022-03-05 16:16:14 -0700

    please fix nodemodules error git gods

--- client/src/components/AppDescription/index.jsx
+++ client/src/components/AppDescription/index.jsx
@@ -2,8 +2,14 @@ import "./styles.css";
 
 export default function AppDescription() {
     return (
+        <div className="appCard">
         <div className="appDescriptionComponent component">
             <h4>Sapling Manifestaion Description</h4>
+            <h3>manifest<br></br> </h3>
+            <h4> ma-nuh-fest | noun</h4>
+            <hr></hr>
+            <p>think it<br></br> believe it<br></br> write it down <br></br> ask the universe for it <br></br> make it happen</p>
+        </div>
         </div>
     )
 }

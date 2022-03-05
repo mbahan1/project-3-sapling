9083e82568e0178c753b8729e679e859462ea3a9
neshmasood <neshtam@gmail.com>
2022-03-04 22:02:21 -0600

    updated the toggle button, but it's still not rending on site.

--- client/src/components/ToggleButton/index.jsx
+++ client/src/components/ToggleButton/index.jsx
@@ -1,25 +1,30 @@
 import {useState} from "react";
 import {ThemeProvider} from "styled-components";
-import { lightTheme, darkTheme, GlobalStyles } from "./themes.js";
+import { lightTheme, darkTheme } from "./themes";
 import './App.css';
-import {Switch} from "antd";
+// import {Switch} from "antd";
+import { GlobalStyles } from "./global";
 
 
 
 
 function ToggleButton() {
-  const[theme, setTheme] = useState ("light");
+    const[theme, setTheme] = useState ('light');
 
-  const themeToggler = () => {
-      theme === "light" ? setTheme("dark") : setTheme("light");
-  };
-
+    const themeToggler = () => {
+        if (theme === 'light') {
+            setTheme('dark');
+        } else {
+            setTheme('light');
+        } 
+    }
 
   return (
-    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme }>
-      <GlobalStyles />
-      <Switch onClick={() => themeToggler()}/>
-        Change the Color
+    <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme }>
+        <>
+            <GlobalStyles />
+            <button onClick={themeToggler}>Toggle Theme</button>
+        </>
       </ThemeProvider>
   );
 

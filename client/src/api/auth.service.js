9a7620d12b5c2aeb56aa452a4349e8acc0121597
jihyekwak <jihyekwak53@gmail.com>
2022-03-05 11:37:28 -0800

    created auth.service.js

---
+++ client/src/api/auth.service.js
@@ -0,0 +1,34 @@
+import tellSaplingTo from "./axios.config";
+
+const auth = "/auth"
+
+const register = (data) => {
+
+}
+
+const login = async (email, password) => {
+    try {
+        return tellSaplingTo
+        .post(`${auth}/login`, {email, password})
+        .then((res) => {
+            console.log(res)
+            if(res.data.token) {
+                localStorage.setItem("user", JSON.stringify(res.data.token))
+            }
+            return res.data.token
+        })
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+const currentUser = () => {
+    let user = localStorage.getItem("user")
+    return JSON.parse(user)
+}
+
+const logout = () => {
+    localStorage.removeItem("user")
+}
+
+export {register, login, currentUser, logout}

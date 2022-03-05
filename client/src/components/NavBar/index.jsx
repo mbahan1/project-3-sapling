ddb647ec9a5ac8b0da8570ac8645db9ce5c91863
jackrobert0220 <jackrobert0220@gmail.com>
2022-03-05 10:49:03 -0800

    prfile page button functions

--- client/src/components/NavBar/index.jsx
+++ client/src/components/NavBar/index.jsx
@@ -8,12 +8,15 @@ export default function NavBar() {
     return(
     <>
     <header className='navbar'>
-    <NavLink to="/" className='navbar__home navbar__item'
+      <NavLink to="/" className='navbar__home navbar__item'
                     style={({ isActive }) => 
                     ({ color: isActive ? 'purple' : 'blue' })}
                     >Home</NavLink>
         {/* <ToggleButton /> */}
-        <div className='navbar__item'>My Profile Page</div>
+      <NavLink to="/Profile" className='navbar__item'
+                    style={({ isActive }) => 
+                    ({ color: isActive ? 'purple' : 'blue' })}
+                    >My Profile Page</NavLink>
         <div className='navbar__item'>Main Feed</div>       
     </header>
     </>
